29cf3f7c1204ef6608506e1b78e706248473cf02
MwangiWambugu <mwangiwambugu@gmail.com>
2025-10-18 21:07:14 +0300

    ui updated

--- authentication/urls.py
+++ authentication/urls.py
@@ -6,10 +6,10 @@ from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path("register/", registrationView.as_view(), name="register"),
+    path("login/", loginView.as_view(), name="login"),
     path("validate_username/",csrf_exempt(UsernameValidationView.as_view()), name="validate_username"),
     path("validate_email/",csrf_exempt(emailValidationView.as_view()), name="validate_email"),
     path("activate/<uidb64>/<token>/", verificationView.as_view(), name="activate"),
-    path("login/", loginView.as_view(), name="login"),
     path("logout/", logoutView.as_view(), name="logout"),
     
 ]
